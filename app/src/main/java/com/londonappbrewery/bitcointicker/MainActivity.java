4862ec06c51daa3845bdd6546dcd86afd4a9213e
PritishSawant <sawantpritish@gmail.com>
2017-09-24 14:57:14 +0530

    Update MainActivity.java

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,16 +12,23 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
+
+import static android.provider.UserDictionary.Words.APP_ID;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
 
     // Member Variables:
     TextView mPriceTextView;
@@ -45,35 +52,96 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                RequestParams requestParams=new RequestParams();
+
+
+
+
+               if(position==0){
+                   String NEW_URL=BASE_URL+"AUD";
+                   letsDoSomeNetworking(NEW_URL);
+               }else if(position==1){
+                  String NEW_URL=BASE_URL+"BRL";
+                   letsDoSomeNetworking(NEW_URL);
+               }else if(position==2){
+                   String NEW_URL=BASE_URL+"CAD";
+                   letsDoSomeNetworking(NEW_URL);
+               }else if(position==3){
+                   String NEW_URL=BASE_URL+"CNY";
+                   letsDoSomeNetworking(NEW_URL);
+               }else if(position==4){
+                   String NEW_URL=BASE_URL+"EUR";
+                   letsDoSomeNetworking(NEW_URL);
+               }else if(position==5){
+                   String NEW_URL=BASE_URL+"GBP";
+                   letsDoSomeNetworking(NEW_URL);
+               }else if(position==6){
+                   String NEW_URL=BASE_URL+"HKD";
+                   letsDoSomeNetworking(NEW_URL);
+               }else if(position==7){
+                   String NEW_URL=BASE_URL+"JPY";
+                   letsDoSomeNetworking(NEW_URL);}
+               else if(position==8){
+                   String NEW_URL=BASE_URL+"PLN";
+                   letsDoSomeNetworking(NEW_URL);
+               }
+               else if(position==9){
+                   String NEW_URL=BASE_URL+"RUB";
+                   letsDoSomeNetworking(NEW_URL);
+               }
+               else if(position==10){
+                   String NEW_URL=BASE_URL+"SEK";
+                   letsDoSomeNetworking(NEW_URL);
+               }
+               else if(position==11){
+                  String NEW_URL=BASE_URL+"USD";
+                   letsDoSomeNetworking(NEW_URL);
+               }
+               else if(position==12){
+                   String NEW_URL=BASE_URL+"ZAR";
+                   letsDoSomeNetworking(NEW_URL);
+               }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.i("Bitcoin", "Nothing selected");
+            }
+        });
 
     }
 
     // TODO: complete the letsDoSomeNetworking() method
     private void letsDoSomeNetworking(String url) {
+        Log.i("Pritish", "letsDoSomeNetworking: "+url);
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler() {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+               
+                DataModel dataModel = DataModel.fromJSON(response);
+                updateUI(dataModel);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+
+                Log.e("ERROR", e.toString());
+                Toast.makeText(MainActivity.this, "Request Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
 
+    private void updateUI(DataModel dataModel){
+        mPriceTextView.setText(dataModel.getAskprice());
 
+    }
 }
